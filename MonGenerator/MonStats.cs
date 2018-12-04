a96d1061ace20b70cc3b02500e036b925bd9a1d2
Alfredo Inostroza <jadenguy@gmail.com>
2018-12-04 12:23:41 -0500

    Test met for generic type

--- MonGenerator/MonStats.cs
+++ MonGenerator/MonStats.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace MonGenerator
 {
     public class MonStats
@@ -5,15 +7,57 @@ namespace MonGenerator
         private int _level;
         private int _attack;
         private int _hp;
-        public int Level { get => _level; private set => _level = value; }
-        public int Attack { get => _attack; private set => _attack = value; }
-        public int Hp { get => _hp; private set => _hp = value; }
+        public int Level
+        {
+            get => _level; private set
+            {
+                var newValue = IntBetween(value, 1, 100);
+                if (newValue != _level)
+                {
+                    _level = newValue;
+                }
+                else { }
+            }
+        }
+        public int Attack
+        {
+            get => _attack; private set
+            {
+                var newValue = IntBetween(value, 1, 255);
+                if (newValue != _level)
+                {
+                    _attack = newValue;
+                }
+                else { }
+            }
+        }
+        public int Hp
+        {
+            get => _hp; private set
+            {
+                var newValue = IntBetween(value, 1, 255);
+                if (newValue != _level)
+                {
+                    _hp = newValue;
+                }
+                else { } //-- Maybe do something with this later?
+            }
+        }
         public MonStats(int initialLevel)
         {
-            _level = initialLevel;
-            _attack = initialLevel;
-            _hp = initialLevel;
-
+            Level = initialLevel;
+            Attack = Level;
+            Hp = Level;
+        }
+        public static int IntBetween(int value, int minimum, int maximum, bool inclusive = true)
+        {
+            int ret;
+            int buffer = 0;
+            if (!inclusive) buffer = 1;
+            if (value > maximum - buffer) ret = maximum - buffer;
+            else if (value < minimum + buffer) ret = minimum + buffer;
+            else ret = value;
+            return ret;
         }
     }
 }
