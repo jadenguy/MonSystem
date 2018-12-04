a978cce95ba19396a0611e5b4dba1f1558fb8c5a
Alfredo Inostroza <jadenguy@gmail.com>
2018-12-04 11:36:48 -0500

    New test to find the level outside of acceptable

--- MonGenerator.Test/MonStatsTest.cs
+++ MonGenerator.Test/MonStatsTest.cs
@@ -5,14 +5,28 @@ namespace MonGenerator.Tests
     [TestFixture]
     public class MonStatsTest
     {
-        [TestCase(1)]
+        [TestCase(-1)]
+        [TestCase(0)]
         [TestCase(99)]
+        [TestCase(100)]
         public void MonStatsTestAttack(int level)
         {
             //arrange
             MonStats stats = new MonStats(level);
-            var expected = level;
 
+            int expected;
+            if (level > 99)
+            {
+                expected = 99;
+            }
+            else if (level < 0)
+            {
+                expected = 0;
+            }
+            else
+            {
+                expected = level;
+            }
             //act
             var actual = stats.Attack;
 
