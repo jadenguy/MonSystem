e75c1c2f47253afd93851f00c34619761ff7580a
Alfredo Inostroza <jadenguy@gmail.com>
2018-12-04 14:11:29 -0500

    Enum instead of magic numbers test

---
+++ MonGenerator/MonStatRanges.cs
@@ -0,0 +1,12 @@
+namespace MonGenerator
+{
+    public enum MonStatRanges
+    {
+        levelMin = 1,
+        levelMax = 100,
+        attackMin=1,
+        attackMax=255,
+        hpMin=1,
+        hpMax=255
+    }
+}
