29bbab8ee66fb648fc6c100ab00d93ce6ae5b70a
Alfredo Inostroza <jadenguy@gmail.com>
2018-12-05 14:04:10 -0500

    updated the 'using' statement for Repository Using IEnumerable

--- Repository/IRepository.cs
+++ Repository/IRepository.cs
@@ -1,5 +1,7 @@
 //-- Based on https://medium.com/falafel-software/implement-step-by-step-generic-repository-pattern-in-c-3422b6da43fd
 
+using System.Collections.Generic;
+
 namespace Repository
 {
     public interface IRepository<T> where T : IEntity
