758eca90dbfc0a6940d95d1990aabd088216723a
Alfredo Inostroza <jadenguy@gmail.com>
2018-12-27 16:08:54 -0500

    Update to single test to verify values are returned and validate mon

--- MonGenerator.Test/MonTypeTest.cs
+++ MonGenerator.Test/MonTypeTest.cs
@@ -10,20 +10,7 @@ namespace MonGenerator.Tests
         [TestCase("newMon", 0, 200, -200, false)]
         [TestCase("newMon", 0, 100, -100, true)]
         [TestCase("newMon", 0, 5, 100, false)]
-        public void MonTypeValidTest(string speciesName, int id, int attack, int hp, bool isValid)
-        {
-            //--Arrange
-            MonType monType = new MonType(speciesName, id, attack, hp);
-            var expected = isValid;
-
-            //--Act
-            var actual = monType.Validate();
-
-            //--Assert
-            Assert.AreEqual(expected, actual);
-        }
-        [TestCase("newMon", 0, 5, 5)]
-        public void MonTypeValuesTest(string speciesName, int id, int attack, int hp)
+        public void MonTypeValuesValidTest(string speciesName, int id, int attack, int hp, bool isValid)
         {
             //--Arrange
             MonType monType = new MonType(speciesName, id, attack, hp);
@@ -31,6 +18,7 @@ namespace MonGenerator.Tests
             var expectedHp = hp;
             var expectedAttack = attack;
             var expectedSpeciesName = speciesName;
+            var expectedValid = isValid;
 
             //--Act
             var actualId = monType.Id;
@@ -44,7 +32,7 @@ namespace MonGenerator.Tests
             Assert.AreEqual(expectedSpeciesName, actualSpeciesName);
             Assert.AreEqual(expectedAttack, actualAttack);
             Assert.AreEqual(expectedHp, actualHp);
-            Assert.IsTrue(actualValid);
+            Assert.AreEqual(expectedValid, actualValid);
         }
     }
 }
