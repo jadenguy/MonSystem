bf24107d3869c9f19a06690b8a5b4d9b88dc52e0
Alfredo Inostroza <jadenguy@gmail.com>
2019-01-02 11:16:18 -0500

    Renamed class Not sure what I'm doing with this exactly. Does species contain stats? Stats doesn't interact with species yet. That should be my next step. Considering moving the intbetween to an extension class

--- src/MonGenerator/Mon.cs
+++ src/MonGenerator/Mon.cs
@@ -5,7 +5,7 @@ namespace MonGenerator
     public class Mon : IEntity
     {
         private int monTypeID;
-        private MonTypeDictionaryRepository monTypeDictionaryRepository;
+        private MonTypeRepository monTypeDictionaryRepository;
 
         public MonType MonType { get => monTypeDictionaryRepository.FindById(monTypeID); set => monTypeDictionaryRepository.Update(value); }
     }
