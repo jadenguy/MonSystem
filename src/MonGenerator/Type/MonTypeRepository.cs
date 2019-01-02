cd8695884447a4318c2daf9948faac8f6ee109bf
Alfredo Inostroza <jadenguy@gmail.com>
2019-01-02 15:52:25 -0500

    MonTypeRepository dummy data Dummy data for testing later

--- src/MonGenerator/Type/MonTypeRepository.cs
+++ src/MonGenerator/Type/MonTypeRepository.cs
@@ -6,5 +6,13 @@ namespace MonGenerator.Type
 {
     public class MonTypeRepository : MonGeneratorRepository<MonType>
     {
+        public MonTypeRepository()
+        {
+            //--  Generating test data for now
+            this.Add(new MonType(speciesName: "base", id: 0, attack: 0, hp: 0));
+            this.Add(new MonType(speciesName: "attacker", id: 1, attack: 100, hp: 0));
+            this.Add(new MonType(speciesName: "tank", id: 2, attack: 0, hp: 100));
+            this.Add(new MonType(speciesName: "glasscannon", id: 2, attack: 100, hp: -100));
+        }
     }
 }
