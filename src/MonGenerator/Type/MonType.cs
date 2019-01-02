a8149fab03b41833ef4d73b10533461ce0aaafa1
Alfredo Inostroza <jadenguy@gmail.com>
2019-01-02 16:34:55 -0500

    Type now includes speed stat and new validation enum

--- src/MonGenerator/Type/MonType.cs
+++ src/MonGenerator/Type/MonType.cs
@@ -1,5 +1,6 @@
 using System;
 using Common.Repository;
+using Common.Math;
 
 namespace MonGenerator.Type
 {
@@ -8,25 +9,30 @@ namespace MonGenerator.Type
         private string _speciesName;
         private int _attack;
         private int _hp;
+        private int _speed;
         public string SpeciesName { get => _speciesName; set => _speciesName = value; }
         public int Attack { get => _attack; set => _attack = value; }
         public int Hp { get => _hp; set => _hp = value; }
-        public int StatSum => Attack + Hp;
-        public MonType(string speciesName, int id, int attack, int hp)
+        public int Speed { get => _speed; set => _speed = value; }
+        public int StatSum => Attack + Hp + Speed;
+        public MonType() { }
+        public MonType(string speciesName, int id, int attack, int hp, int speed)
         {
             SpeciesName = speciesName;
             Id = id;
             Attack = attack;
             Hp = hp;
+            Speed = speed;
         }
         public override bool Validate()
         {
             var idValid = (Id >= 0);
             var speciesNameValid = !String.IsNullOrWhiteSpace(SpeciesName);
-            var hpValid = (Hp <= 100 && Hp >= -100);
-            var attackValid = (Attack <= 100 && Attack >= -100);
-            var totalStatsValid = (StatSum <= 100);
-            var ret = idValid && hpValid && attackValid && totalStatsValid && speciesNameValid;
+            var hpValid = Hp.IsBetweenII((int)MonTypeRanges.hpMin, (int)MonTypeRanges.hpMax);
+            var attackValid = Attack.IsBetweenII((int)MonTypeRanges.attackMin, (int)MonTypeRanges.attackMax);
+            var speedValid = Speed.IsBetweenII((int)MonTypeRanges.speedMin, (int)MonTypeRanges.speedMax);
+            var totalStatsValid = StatSum.IsBetweenII((int)MonTypeRanges.statTotalMin, (int)MonTypeRanges.statTotalMax);
+            var ret = idValid && hpValid && attackValid && speedValid && totalStatsValid && speciesNameValid;
             return ret;
         }
     }
--- src/MonGenerator/Type/MonTypeRepository.cs
+++ src/MonGenerator/Type/MonTypeRepository.cs
@@ -9,10 +9,14 @@ namespace MonGenerator.Type
         public MonTypeRepository()
         {
             //--  Generating test data for now
-            this.Add(new MonType(speciesName: "base", id: 0, attack: 0, hp: 0));
-            this.Add(new MonType(speciesName: "attacker", id: 1, attack: 100, hp: 0));
-            this.Add(new MonType(speciesName: "tank", id: 2, attack: 0, hp: 100));
-            this.Add(new MonType(speciesName: "glasscannon", id: 2, attack: 100, hp: -100));
+            this.Add(new MonType(speciesName: "base", id: 0, attack: 0, hp: 0, speed: 0));
+            this.Add(new MonType(speciesName: "attacker", id: 1, attack: 50, hp: 0, speed: 0));
+            this.Add(new MonType(speciesName: "tank", id: 2, attack: 0, hp: 50, speed: 0));
+            this.Add(new MonType(speciesName: "speedy", id: 3, attack: 0, hp: 0, speed: 50));
+            this.Add(new MonType(speciesName: "glassCannon", id: 5, attack: 100, hp: -50, speed: 0));
+            this.Add(new MonType(speciesName: "glacier", id: 6, attack: 0, hp: 100, speed: -50));
+            this.Add(new MonType(speciesName: "lightningGlassCannon", id: 7, attack: 100, hp: -100, speed: 50));
+            this.Add(new MonType(speciesName: "fodder", id: -1, attack: -100, hp: -100, speed: -50));
         }
     }
 }
