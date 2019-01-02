d2c1e4339f2c6227a1e2b47bec61f8d1db064731
Alfredo Inostroza <jadenguy@gmail.com>
2019-01-02 16:40:43 -0500

    Testing four other methods

--- tests/Common.Test/BetweenExtensionTest.cs
+++ tests/Common.Test/BetweenExtensionTest.cs
@@ -35,5 +35,31 @@ namespace Common.Math.Tests
             //--Assert
             Assert.AreEqual(expected, actual);
         }
+        [TestCase(1, 5, 3, 3)]
+        [TestCase(1, 5, 5, 4)]
+        [TestCase(1, 5, 1, 1)]
+        [TestCase(1, 5, null, 1)]
+        public void intInBetweenIETest(int low, int high, int value, int expectedValue)
+        {
+            //--Arrange
+            var expected = expectedValue;
+            //--Act
+            var actual = value.IntBetweenIE(low, high);
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestCase(1, 5, 3, 3)]
+        [TestCase(1, 5, 5, 5)]
+        [TestCase(1, 5, 1, 2)]
+        [TestCase(1, 5, null, 2)]
+        public void intInBetweenEITest(int low, int high, int value, int expectedValue)
+        {
+            //--Arrange
+            var expected = expectedValue;
+            //--Act
+            var actual = value.IntBetweenEI(low, high);
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
