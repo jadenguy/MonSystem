64df935524743be491833058fba04606e529e007
Alfredo Inostroza <jadenguy@gmail.com>
2019-01-03 14:30:28 -0500

    ConsoleApp is new console app namespace Just cleaner Should this be under mon.console namespace?

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(GlobalExceptionHandler);
+            System.Console.WriteLine("Hello World!");
+            return 0;
+        }
+        static void GlobalExceptionHandler(object sender, EventArgs args)
+        {
+            System.Console.WriteLine("Exception");
+            Environment.Exit(1);
+        }
+    }
+}
