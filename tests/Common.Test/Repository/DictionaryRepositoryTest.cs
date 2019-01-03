589c3e4c7777643a094ca256bfcbfa0d41a5a829
Alfredo Inostroza <jadenguy@gmail.com>
2019-01-03 09:30:02 -0500

    Dictionary Repository Test Tests that the dictionary repository works as it should

---
+++ tests/Common.Test/Repository/DictionaryRepositoryTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Common.Repository;
+using NUnit.Framework;
+
+namespace Common.Repository.Tests
+{
+    public class TestEntity : IEntity
+    {
+        public TestEntity(int id) : base(id) { }
+    }
+    [TestFixture]
+    public class DictionaryRepositoryTest
+    {
+        [Test]
+        public void AddMemberTest()
+        {
+            //-- Arrange
+            var dictionaryRepository = new DictionaryRepository<TestEntity>();
+            var testEntity = new TestEntity(0);
+            var expected = testEntity.Id;
+
+            //-- Act
+            dictionaryRepository.Add(testEntity);
+            var actual = dictionaryRepository.FindById(testEntity.Id).Id;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
