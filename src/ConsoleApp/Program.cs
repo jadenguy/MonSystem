aa2528950bc4ca50ed066e0cd7e63dd96d390766
Alfredo Inostroza <jadenguy@gmail.com>
2019-01-04 09:30:05 -0500

    Messing around with global exception handlers 1 in 20 chance of not failing

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -8,11 +8,31 @@ namespace ConsoleApp
         {
             AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(GlobalExceptionHandler);
             System.Console.WriteLine("Hello World!");
+            SavingThrow();
+
             return 0;
+        }
+
+        private static void SavingThrow()
+        {
+            var x = new Random();
+            var diceRoll = x.Next(20) + 1;
+            System.Console.WriteLine(diceRoll);
+            if (diceRoll <= 19)
+            {
+                var ex = new System.Exception("You die");
+                throw ex;
+            }
+            else
+            {
+                System.Console.WriteLine("You win the saving throw");
+            }
         }
         static void GlobalExceptionHandler(object sender, EventArgs args)
         {
-            System.Console.WriteLine("Exception");
+            System.Console.WriteLine("Exception detected");
+            System.Console.WriteLine(sender);
+            System.Console.WriteLine(args);
             Environment.Exit(1);
         }
     }
