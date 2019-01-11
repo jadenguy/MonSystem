a61ca44ce2a6dd315cd1f3eb367358ece187cf39
Alfredo Inostroza <jadenguy@gmail.com>
2019-01-11 14:49:47 -0500

    YouLoseException cleanup Really only need 1 ctor

--- src/Common/Exception/YouLoseException.cs
+++ src/Common/Exception/YouLoseException.cs
@@ -2,8 +2,6 @@ namespace Common.Exception
 {
     public class YouLoseException : System.Exception
     {
-        public YouLoseException() { }
         public YouLoseException(string message) : base(message) { }
-        public YouLoseException(string message, System.Exception inner) : base(message, inner) { }
     }
 }
