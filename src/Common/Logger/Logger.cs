76cdbb4928b300670db5d545ec9264aa2cddb2ae
Alfredo Inostroza <jadenguy@gmail.com>
2019-01-11 10:41:20 -0500

    Logger class now has single method Logger class method calls an event Logger class assigns handlers to the event

--- src/Common/Logger/Logger.cs
+++ src/Common/Logger/Logger.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace Common.Logger
@@ -8,69 +9,102 @@ namespace Common.Logger
     public class Logger
     {
         private DateTime opened = DateTime.Now;
-        private event EventHandler<LoggerEventArgs> _logThis;
-        public event EventHandler<LoggerEventArgs> LogEvent
+
+        public bool WriteFile
+        {
+            get => IsMethodInDelegate(new EventHandler<LoggerEventArgs>(FileWrite));
+
+            set
+            {
+                var del = new EventHandler<LoggerEventArgs>(FileWrite);
+                if (value)
+                { AddDelegateOnlyOnce(del); }
+            }
+        }
+        public bool WriteConsole
+        {
+            get => IsMethodInDelegate(new EventHandler<LoggerEventArgs>(ConsoleWrite));
+
+            set
+            {
+                var del = new EventHandler<LoggerEventArgs>(ConsoleWrite);
+                if (value)
+                { AddDelegateOnlyOnce(del); }
+            }
+        }
+        public bool WriteDebug
+        {
+            get => IsMethodInDelegate(new EventHandler<LoggerEventArgs>(DebugWrite));
+
+            set
+            {
+                var del = new EventHandler<LoggerEventArgs>(DebugWrite);
+                if (value)
+                { AddDelegateOnlyOnce(del); }
+            }
+        }
+        private void AddDelegateOnlyOnce(EventHandler<LoggerEventArgs> del)
+        {
+            if (!IsMethodInDelegate(del))
+            {
+                LogEvent -= del;
+                LogEvent += del;
+            }
+        }
+        private bool IsMethodInDelegate(EventHandler<LoggerEventArgs> del)
+        {
+            return _logThis?.GetInvocationList()?.
+                                Where(d => d.Equals(del))?.
+                                Count() > 0;
+        }
+
+        public event EventHandler<LoggerEventArgs> _logThis;
+        private event EventHandler<LoggerEventArgs> LogEvent
         {
             [MethodImpl(MethodImplOptions.Synchronized)]
             add => _logThis = (EventHandler<LoggerEventArgs>)Delegate.Combine(_logThis, value);
             [MethodImpl(MethodImplOptions.Synchronized)]
             remove => _logThis = (EventHandler<LoggerEventArgs>)Delegate.Remove(_logThis, value);
         }
-
         private string path = "file.log";
-        private string now => DateTime.Now.ToString("o");
-        public Logger() { }
+        private string nowString => DateTime.Now.ToString("o");
+        private Logger() { }
         public Logger(string filepath)
         {
             path = filepath;
         }
-        public DateTime Opened { get => opened; }
-        public string Path
+        private DateTime Opened { get => opened; }
+        private string Path
         {
-            get => path; set { if (path != value) path = value; }
+            get => path; set { if (string.IsNullOrWhiteSpace(value) && path != value) path = value; }
         }
-        public void FileWrite(object sender, LoggerEventArgs e)
+        private void FileWrite(object sender, LoggerEventArgs e)
         {
             string senderString = string.Empty;
-            if (sender != null)
+            if (sender != null && !string.IsNullOrWhiteSpace(sender.ToString()))
             {
-                var notBlank = !string.IsNullOrWhiteSpace(sender.ToString());
-                if (notBlank)
-                {
-                    senderString = $" [{(sender.ToString())}]";
-                }
+                senderString = $" [{(sender.ToString())}]";
             }
-            var lines = $"{now}{senderString}: {e}";
+            var lines = $"{nowString}{senderString}: {e}";
             TextWriter textWriter = new StreamWriter(path, append: true);
             textWriter.WriteLineAsync(lines);
             textWriter.Close();
         }
-        public void ConsoleWrite(object sender, LoggerEventArgs e)
+        private void ConsoleWrite(object sender, LoggerEventArgs e)
         {
             Console.WriteLine(e);
         }
-        public void DebugWrite(object sender, LoggerEventArgs e)
+        private void DebugWrite(object sender, LoggerEventArgs e)
         {
             Debug.WriteLine(e);
-        }
-        public void LogThis(string logMessage)
-        {
-            LogThis(new object(), new LoggerEventArgs(logMessage));
         }
         public void LogThis(object sender, string logMessage)
         {
             LogThis(sender, new LoggerEventArgs(logMessage));
         }
-        public void LogThis<T>(object sender, T eventArgs) where T : EventArgs
+        private void LogThis(object sender, LoggerEventArgs loggerEventArgs)
         {
-            LogThis(sender, eventArgs.ToString());
-        }
-        public void LogThis(object sender, LoggerEventArgs eventArgs)
-        {
-            if (_logThis != null)
-            {
-                _logThis(sender, eventArgs);
-            }
+            _logThis?.Invoke(sender, loggerEventArgs);
         }
     }
 }
--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -6,43 +6,54 @@ namespace ConsoleApp
 {
     class Program
     {
-        private static object mainObject = "program";
-
-        private static Logger logger = new Logger("file.log");
+        private static object mainObject;
+        private static Logger _logger = new Logger("file.log");
         static int Main()
         {
-            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(GlobalExceptionHandler);
-            logger.LogEvent += logger.FileWrite;
-            logger.LogEvent += logger.ConsoleWrite;
-            logger.LogEvent += logger.DebugWrite;
-            Log("Starting");
+            AppDomain.CurrentDomain.UnhandledException += GlobalExceptionHandler;
+            System.Threading.Tasks.TaskScheduler.UnobservedTaskException += GlobalExceptionHandler;
+            mainObject = nameof(Main);
+            _logger.WriteFile = true;
+            _logger.WriteConsole = true;
+            _logger.WriteDebug = true;
             Log("Starting");
             Log("You attempt to hack the computer. Roll for initiative.");
-            var Dice = new Dice();
-            Dice.LoseEvent += (o, e) => logger.LogThis(o,e);
+            var Dice = new Dice(null);
+            Dice.LoseEvent += ThrowException;
             var throwResult = Dice.SavingThrow(20);
             Log(throwResult);
             Log("Closing");
             return 0;
         }
-
         private static void Log(string message)
         {
-            logger.LogThis(mainObject, message);
+            _logger.LogThis(mainObject, message);
         }
-
         static void GlobalExceptionHandler(object sender, EventArgs args)
         {
-            Log("Exception detected");
-            Log(sender.ToString());
-            Log(args.ToString());
-            var ex = args as UnhandledExceptionEventArgs;
-            if (ex != null)
+            try
             {
-                Log(ex.ExceptionObject.ToString());
+                System.Console.WriteLine("GlobalExceptionHandler");
+                Log("Exception detected");
+                Log(sender.ToString());
+                Log(args.ToString());
+                var ex = args as UnhandledExceptionEventArgs;
+                if (ex != null)
+                {
+                    Log(ex.ExceptionObject.ToString());
+                }
+                Log("Closing Unexpectedly");
             }
-            Log("Closing Unexpectedly");
-            Environment.Exit(1);
+            catch (System.Exception)
+            {
+                System.Console.WriteLine("GlobalExceptionHandlerFail");
+            }
+            finally
+            {
+                System.Console.WriteLine("GlobalExceptionHandlerDone");
+                Environment.Exit(1);
+            }
         }
+        static void ThrowException(object sender, string eventArgs) => throw new Exception(eventArgs.ToString());
     }
 }
